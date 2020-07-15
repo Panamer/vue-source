295f16f5ce7953b7d7e6f4ae5342d35a154a4c29
guoyongwei <guoyongwei@gomeholdings.com.cn>
2020-07-15 22:34:12 +0800

    依赖收集

--- src/lifecycle.js
+++ src/lifecycle.js
@@ -1,41 +1,22 @@
 import Watcher from './observer/watcher';
 import { patch } from './vdom/patch';
 export function lifeCycleMixin(Vue) {
-    Vue.prototype._update = function(vnode) {
+    Vue.prototype._update = function(vnode){
+        // 传入vnode 得到真实dom  挂在vm.$el 上
         const vm = this;
-
-        // 将虚拟节点 变成 真实节点 替换掉$el
-
-        // 后续 dom diff 也会执行此方法
-
-
-        vm.$el = patch(vm.$el, vnode);
-
+        vm.$el = patch(vm.$el, vnode)
     }
 }
 export function mountComponent(vm, el) {
-    // Vue在渲染的过程中 会创建一个 所谓的“渲染watcher ” 只用来渲染的
-    // watcher就是一个回调 每次数据变化 就会重新执行watcher
 
-
-    // Vue是不是MVVM框架
     callHook(vm, 'beforeMount')
-    const updateComponent = () => {
-        // 内部会调用刚才我们解析后的render方法 =》 vnode
-        // _render => options.render 方法
-        // _update => 将虚拟dom 变成真实dom 来执行
-        console.log('update')
-        vm._update(vm._render());
+
+    function updateComponent(){
+        vm._update(vm._render())
     }
+    new Watcher(vm, updateComponent, ()=>{}, true)
 
-    // 每次数据变化 就执行 updateComponent 方法 进行更新操作
-    new Watcher(vm, updateComponent, () => {}, true);
-
-    callHook(vm, 'mounted');
-
-
-
-    // vue 响应式数据的规则 数据变了 视图会刷新
+    callHook(vm, 'mounted')
 }
 
 
--- src/observer/watcher.js
+++ src/observer/watcher.js
@@ -9,42 +9,33 @@ class Watcher {
         this.vm = vm;
         this.exprOrFn = exprOrFn;
         this.cb = cb;
-        this.options = options;
-        this.deps = []; // 这个watcher会存放所有的dep
-        this.depsId = new Set();
-        if (typeof exprOrFn == 'function') {
-            this.getter = exprOrFn;
+        this.options = options
+        this.deps = []
+        this.depsId = new set()
+        if (typeof exprOrFn === 'function') {
+            this.getter = exprOrFn
         }
-
-        this.id = id++;
-        this.get();
+        this.id = id++
+        this.get()
     }
     run(){
-        this.get(); // 重新渲染
+        this.get()
     }
-    get() {
-        // 1.是先把渲染watcher 放到了 Dep.target上
-        // 2.this.getter()  是不是去页面取值渲染  就是调用defineProperty的取值操作
-        // 3.我就获取当前全局的Dep.target,每个属性都有一个dep 取值是就将Dep.target 保留到当前的dep中
-        // 4.数据变化 通知watcher 更新 
-
-        pushTarget(this); // 在取值之前 将watcher先保存起来
-        this.getter(); // 这句话就实现了视图的渲染  -》 操作是取值 
-        popTarget(); // 删掉watcher
-
-        // Vue是组件级别更新的
+    get(){
+        pushTarget(this)
+        this.getter()
+        popTarget()
     }
-    addDep(dep) {
-        let id = dep.id;
+    addDep(dep){
+        let id = dep.id
         if (!this.depsId.has(id)) {
-            this.depsId.add(id);
+            this.depsId.add(id)
             this.deps.push(dep);
-            dep.addSub(this); // 让当前dep 订阅这个watcher
+            dep.addSub(this)
         }
-    } 
-    update(){ // 更新原理
-        queueWatcher(this); // 将watcher存储起来
-        // this.get();  // 以前调用get方法是直接更新视图
+    }
+    update(){
+        queueWatcher(this)
     }
 }
 
