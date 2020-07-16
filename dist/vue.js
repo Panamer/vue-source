7b9b7f678689fe08aa46e99ee0564b8bc726f638
guoyongwei <guoyongwei@gomefinance.com.cn>
2020-07-16 17:23:56 +0800

    initGlobalAPI 给构造函数扩展全局的方法

--- dist/vue.js
+++ dist/vue.js
@@ -148,6 +148,65 @@
   function isObject(obj) {
     return _typeof(obj) === 'object' && obj !== null;
   }
+  var LIFECYCLE_HOOKS = ['beforeCreate', 'created', 'mounted', 'beforeUpdate', 'updated'];
+  var strats = {};
+
+  function mergeHook(parentVal, childVal) {
+    // []
+    if (childVal) {
+      // 如果 孩子有值 
+      if (parentVal) {
+        // 父亲有值 就直接拼接
+        return parentVal.concat(childVal);
+      } else {
+        // 如果孩子有值父亲没值 就将孩子包装成数组
+        return [childVal]; // Vue.options.beforeCreate = [childVal]
+      }
+    } else {
+      return parentVal; // 直接返回父亲，因为没有孩子  
+    }
+  }
+
+  LIFECYCLE_HOOKS.forEach(function (hook) {
+    strats[hook] = mergeHook;
+  });
+  function mergeOptions(parent, child) {
+    // {...parent,...child}  {a:1.b:2}  {a:{a:{b:2}}}
+    var options = {}; // 如果父亲和儿子里都有一个属性 这个属性不冲突 
+
+    for (var key in parent) {
+      // 处理父亲的所有属性
+      mergeField(key);
+    }
+
+    for (var _key in child) {
+      // 处理儿子的所有属性，如果父亲有的值 在第一个循环中就已经处理了
+      if (!parent.hasOwnProperty(_key)) {
+        mergeField(_key);
+      }
+    }
+
+    function mergeField(key) {
+      // 两个组件间 data是函数 
+      // 写代码时很忌讳 各种if else if else 
+      // 策略模式 根据不同的属性 调用不同的策略  
+      if (strats[key]) {
+        // 这里就包含了 mergeHook的逻辑
+        options[key] = strats[key](parent[key], child[key]);
+      } else if (isObject(parent[key]) && isObject(child[key])) {
+        options[key] = Object.assign(parent[key], child[key]);
+      } else {
+        if (child[key] == null) {
+          options[key] = parent[key];
+        } else {
+          options[key] = child[key]; // 用儿子的值 直接覆盖掉 父亲的值
+        }
+      }
+    } // 面试时 经常会提到对象间的合并
+
+
+    return options;
+  }
 
   // 每个dep也有一个自己的id 和watcher一样
   // dep和watcher是 多对多的关系 
@@ -1079,6 +1138,18 @@
     };
   }
 
+  // Vue.directive Vue.filter Vue.component
+  function initGlobalAPI(Vue) {
+    // 全局api 肯定接受很多参数
+    Vue.options = {}; // 所有的全局api 用户传递的参数 都会绑定到这个对象中 
+    // 提取公共的方法 逻辑，通过此方法混合到每个实例中
+
+    Vue.mixin = function (mixin) {
+      this.options = mergeOptions(this.options, mixin);
+      console.log(this.options);
+    };
+  }
+
   /**
    * vue使用的是rollup打包 它的特点是把多个小模块 组合成复杂的逻辑
    * rollup更专注于打包javascript类库
@@ -1098,6 +1169,10 @@
   renderMixin(Vue); // vm._render
 
   lifeCycleMixin(Vue); // _update
+  // 给构造函数扩展全局的方法
+
+  initGlobalAPI(Vue);
+  Vue.prototype.$nextTick = nextTick;
 
   return Vue;
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@
 import { initMixin } from './init'
 import { renderMixin } from './render.js'
 import { lifeCycleMixin } from './lifecycle.js'
+import {initGlobalAPI} from './global-api/index.js'
+import {nextTick} from './observer/scheduler'
 
 function Vue(options) {
     // 内部要进行初始化操作
@@ -19,6 +21,11 @@ function Vue(options) {
 initMixin(Vue)  // initState $mount 
 renderMixin(Vue)    // vm._render
 lifeCycleMixin(Vue) // _update
+
+
+// 给构造函数扩展全局的方法
+initGlobalAPI(Vue)
+Vue.prototype.$nextTick = nextTick
 
 
 
